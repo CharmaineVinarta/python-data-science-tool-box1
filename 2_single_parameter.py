31626f87ccef457327501e71e0ce62174183050d
Charmaine Vinarta <120625052+CharmaineVinarta@users.noreply.github.com>
2023-10-24 16:30:23 -0700

    Create 2_single_parameter.py

--- 2_single_parameter.py
+++ 2_single_parameter.py
@@ -7,7 +7,7 @@
 def shout(word):
     """Print a string with three exclamation marks"""
     # Concatenate the strings: shout_word
-    shout_word = 'word' + '!!!'
+    shout_word = word + '!!!'
 
     # Print shout_word
     print(shout_word)
